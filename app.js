970b0115608e59a7c5f3ee74e8f0f4a04687add7
SionOwen <sionowen34@gmail.com>
2015-02-20 15:28:10 -0700

    successfully implemented the Post/Redirect/Get technique

--- app.js
+++ app.js
@@ -6,11 +6,18 @@ app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: false}));
-
-app.get('/', function(req, res) {
-	res.render('index');
+app.post('/formsubmit', function(req, res) {
+	res.redirect('/success');
 });
-
+app.get('/', function(req, res) {
+	res.send('<form method="post" action="/formsubmit">'+
+		'<input name="email" type="email">'+
+		'<input type="submit">'+
+		'</form>');
+});
+app.get('/success', function(req, res){
+	res.send('success')
+});
 var server = app.listen(5950, function() {
 	console.log('Express server listening on port ' + server.address().port);
 });
